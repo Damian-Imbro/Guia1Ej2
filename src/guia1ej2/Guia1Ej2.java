5a90887bcac4387d3df128f53813cd3ad4ea3c99
Damian Imbrogiano <120312087+Damian-Imbro@users.noreply.github.com>
2023-08-08 21:06:55 -0300

    Update Guia1Ej2.java

--- src/guia1ej2/Guia1Ej2.java
+++ src/guia1ej2/Guia1Ej2.java
@@ -16,7 +16,7 @@ public class Guia1Ej2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Primer commin en GitHub");
+        System.out.println("Primer commint en GitHub");
     }
     
 }
